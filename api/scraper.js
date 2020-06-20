1a3cd4d3fa75d1fe5b5b60ddb417ec90d0db5c20
Vipul Chaudhary <vpul.chaudhary@gmail.com>
2020-06-21 00:42:33 +0545

    add headers to behave like a browser

--- api/scraper.js
+++ api/scraper.js
@@ -40,7 +40,13 @@ const scraper = async (targetUrl) => {
     const response = await axios(targetUrl, {
       headers: {
         'User-Agent':
-          'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/77.0.1',
+          'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:77.0) Gecko/20100101 Firefox/77.0',
+        Accept:
+          'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Accept-Encoding': 'gzip',
+        DNT: '1',
+        Connection: 'close',
       },
       maxContentLength: 10000000, // 10 mb
     });
